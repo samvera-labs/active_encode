886891f9bf6b6a4d61960b0e4a9bf7470d7fd4de
Chris Colvard <cjcolvar@indiana.edu>
2015-05-08 10:41:50 -0400

    Update example

--- examples/options.rb
+++ examples/options.rb
@@ -1,20 +1,16 @@
-require 'hydra-transcoder'
+require 'active-encode'
 
 class PresetOutputs < ActiveEncode::Base
-  def default_options
+  def default_options(input)
     {preset: 'avalon-skip-transcoding'}
-  end
-
-  def default_outputs
-    {:hls_high, :hls_med, :hls_low, :rtmp_high, :rtmp_med, :rtmp_low}
   end
 end
 
 PresetOutputs.create(File.open('/path/to/awesome/video.mp4'))
 
 class HLSOnlyOutputs < PresetOutputs
-  def default_outputs
-    {:hls_high, :hls_med, :hls_low}
+  def default_options(input)
+    super.merge({outputs: [:hls_high, :hls_med, :hls_low]})
   end
 end
 
