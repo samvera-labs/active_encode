3e03ecd6bb98acdc73129c9cf129fd85913bad23
Chris Colvard <cjcolvar@indiana.edu>
2017-06-13 17:18:22 -0400

    Update test

--- spec/units/core_spec.rb
+++ spec/units/core_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe ActiveEncode::Core do
   describe 'find' do
     it "raises NotFound when no id is supplied" do
-      expect { ActiveEncode::Base.find(nil) }.to raise_error(ActiveEncode::NotFound)
+      expect { ActiveEncode::Base.find(nil) }.to raise_error(ArgumentError)
     end
   end
 end
