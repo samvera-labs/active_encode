5a07225b27e8d936b47c7b3dd08c751ad26564d1
Chris Colvard <cjcolvar@indiana.edu>
2018-08-21 13:27:16 -0400

    Merge results from engine adapter into current object

--- lib/active_encode/engine_adapters/test_adapter.rb
+++ lib/active_encode/engine_adapters/test_adapter.rb
@@ -6,14 +6,15 @@ module ActiveEncode
       end
 
       def create(encode)
-        encode.id = SecureRandom.uuid
-        @encodes[encode.id] = encode
-        encode.state = :running
-        encode
+        new_encode = encode.dup
+        new_encode.id = SecureRandom.uuid
+        new_encode.state = :running
+        @encodes[new_encode.id] = new_encode
+        new_encode
       end
 
       def find(id, _opts = {})
-        @encodes[id]
+        @encodes[id].dup
       end
 
       def list(*_filters)
@@ -21,9 +22,10 @@ module ActiveEncode
       end
 
       def cancel(encode)
-        e = @encodes[encode.id]
-        e.state = :cancelled
-        e
+        new_encode = @encodes[encode.id].dup
+        new_encode.state = :cancelled
+        @encodes[encode.id] = new_encode
+        new_encode
       end
 
       def purge(encode)
--- spec/units/core_spec.rb
+++ spec/units/core_spec.rb
@@ -1,9 +1,116 @@
 require 'spec_helper'
 
 describe ActiveEncode::Core do
+  before do
+    class CustomEncode < ActiveEncode::Base
+    end
+  end
+   after do
+    Object.send(:remove_const, :CustomEncode)
+  end
+
+  let(:encode_class) { ActiveEncode::Base }
+
   describe 'find' do
-    it "raises NotFound when no id is supplied" do
-      expect { ActiveEncode::Base.find(nil) }.to raise_error(ArgumentError)
+    let(:id) { encode_class.create(nil).id }
+    subject { encode_class.find(id) }
+
+    it { is_expected.to be_a encode_class }
+    its(:id) { is_expected.to eq id }
+
+    context 'with no id' do
+      let(:id) { nil }
+
+      it 'raises an error' do
+        expect { subject }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'with an ActiveEncode::Base subclass' do
+      let(:encode_class) { CustomEncode }
+
+      it { is_expected.to be_a encode_class }
+      its(:id) { is_expected.to eq id }
+
+      context 'casting', :skip do
+        let(:id) { ActiveEncode::Base.create(nil).id }
+
+        it { is_expected.to be_a encode_class }
+        its(:id) { is_expected.to eq id }
+      end
+    end
+  end
+
+  describe 'create' do
+    subject { encode_class.create(nil) }
+
+    it { is_expected.to be_a encode_class }
+    its(:id) { is_expected.not_to be nil }
+    its(:state) { is_expected.not_to be nil }
+
+    context 'with an ActiveEncode::Base subclass' do
+      let(:encode_class) { CustomEncode }
+
+      it { is_expected.to be_a encode_class }
+      its(:id) { is_expected.not_to be nil }
+      its(:state) { is_expected.not_to be nil }
+    end
+  end
+
+  describe '#create!' do
+    let(:encode) { encode_class.new(nil) }
+    subject { encode.create! }
+
+    it { is_expected.to equal encode }
+    it { is_expected.to be_a encode_class }
+    its(:id) { is_expected.not_to be nil }
+    its(:state) { is_expected.not_to be nil }
+
+    context 'with an ActiveEncode::Base subclass' do
+      let(:encode_class) { CustomEncode }
+
+      it { is_expected.to equal encode }
+      it { is_expected.to be_a encode_class }
+      its(:id) { is_expected.not_to be nil }
+      its(:state) { is_expected.not_to be nil }
+    end
+  end
+
+  describe '#cancel!' do
+    let(:encode) { encode_class.create(nil) }
+    subject { encode.cancel! }
+
+    it { is_expected.to equal encode }
+    it { is_expected.to be_a encode_class }
+    its(:id) { is_expected.not_to be nil }
+    it { is_expected.to be_cancelled }
+
+    context 'with an ActiveEncode::Base subclass' do
+      let(:encode_class) { CustomEncode }
+
+      it { is_expected.to equal encode }
+      it { is_expected.to be_a encode_class }
+      its(:id) { is_expected.not_to be nil }
+      it { is_expected.to be_cancelled }
+    end
+  end
+
+  describe '#reload' do
+    let(:encode) { encode_class.create(nil) }
+    subject { encode.reload }
+
+    it { is_expected.to equal encode }
+    it { is_expected.to be_a encode_class }
+    its(:id) { is_expected.not_to be nil }
+    its(:state) { is_expected.not_to be nil }
+
+    context 'with an ActiveEncode::Base subclass' do
+      let(:encode_class) { CustomEncode }
+
+      it { is_expected.to equal encode }
+      it { is_expected.to be_a encode_class }
+      its(:id) { is_expected.not_to be nil }
+      its(:state) { is_expected.not_to be nil }
     end
   end
 end
