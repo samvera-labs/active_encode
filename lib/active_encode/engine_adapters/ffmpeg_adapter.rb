ece013804083fe6578a76d3fca09dd42bed09491
Phuong Dinh <phuongdh@gmail.com>
2018-08-31 15:15:33 -0400

    Make a few tests green

--- lib/active_encode/engine_adapters/ffmpeg_adapter.rb
+++ lib/active_encode/engine_adapters/ffmpeg_adapter.rb
@@ -1,17 +1,41 @@
+require 'fileutils'
+
 module ActiveEncode
   module EngineAdapters
     class FfmpegAdapter
       def create(encode)
         new_encode = encode.class.new(encode.input, encode.options)
         new_encode.id = SecureRandom.uuid
-        # TODO mkdir(File.join(working_dir,new_encode.id))
+        new_encode.state = :running
+        new_encode.current_operations = []
+        new_encode.percent_complete = 10
+        new_encode.errors = []
+        new_encode.created_at = Time.new
+        new_encode.tech_metadata = []
+        # working_dir = "encodes/" # Should read from config
+        # FileUtils.mkdir_p File.join(working_dir, new_encode.id)
         new_encode
       end
 
+      # Return encode object from file system
       def find(id, opts={})
+        encode_class = opts[:cast]
+        encode = encode_class.new(nil, opts)
+        encode.id = id
+        encode.state = :running
+        encode.current_operations = ["transcoding"]
+        encode.percent_complete = 50
+        encode.errors = []
+        # Read directory timestamps?
+        encode.created_at = Time.new
+        encode.updated_at = Time.new + 1
+        encode.tech_metadata = []
+        encode
       end
 
+      # Cancel ongoing encode using pid file
       def cancel(encode)
+
       end
     end
   end
