df8b3ada6dedeaec16887f482b3f7c00ceb22cdc
Chris Colvard <cjcolvar@indiana.edu>
2018-09-05 12:09:11 -0400

    Matterhorn adapter compliant with updated shared specs

--- lib/active_encode/engine_adapters/matterhorn_adapter.rb
+++ lib/active_encode/engine_adapters/matterhorn_adapter.rb
@@ -53,22 +53,28 @@ module ActiveEncode
 
         def build_encode(workflow)
           return nil if workflow.nil?
-          encode = ActiveEncode::Base.new(convert_input(workflow), convert_options(workflow))
+          input_url = convert_input(workflow)
+          input_url = get_workflow_title(workflow) if input_url.blank?
+          encode = ActiveEncode::Base.new(input_url, convert_options(workflow))
           encode.id = convert_id(workflow)
           encode.state = convert_state(workflow)
           encode.current_operations = convert_current_operations(workflow)
           encode.percent_complete = calculate_percent_complete(workflow)
           encode.created_at = convert_created_at(workflow)
-          encode.updated_at = convert_updated_at(workflow)
+          encode.updated_at = convert_updated_at(workflow) || encode.created_at
           encode.output = convert_output(workflow, encode.options)
           encode.errors = convert_errors(workflow)
 
+          encode.input.id = "presenter/source"
+          encode.input.state = encode.state
+          encode.input.created_at = encode.created_at
+          encode.input.updated_at = encode.updated_at
           tech_md = convert_tech_metadata(workflow)
-          [:width, :height, :frame_rate, :checksum, :audio_codec, :video_codec,
+          [:width, :height, :duration, :frame_rate, :checksum, :audio_codec, :video_codec,
            :audio_bitrate, :video_bitrate].each do |field|
             encode.input.send("#{field}=", tech_md[field])
           end
-          
+
           encode
         end
 
@@ -76,8 +82,12 @@ module ActiveEncode
           workflow.attribute('id').to_s
         end
 
+        def get_workflow_state(workflow)
+          workflow.attribute('state').to_s
+        end
+
         def convert_state(workflow)
-          case workflow.attribute('state').to_s
+          case get_workflow_state(workflow)
           when "INSTANTIATED", "RUNNING" # Should there be a queued state?
             :running
           when "STOPPED"
@@ -91,7 +101,11 @@ module ActiveEncode
 
         def convert_input(workflow)
           # Need to do anything else since this is a MH url? and this disappears when a workflow is cleaned up
-          workflow.xpath('mediapackage/media/track[@type="presenter/source"]/url/text()').to_s
+          workflow.xpath('mediapackage/media/track[@type="presenter/source"]/url/text()').to_s.strip
+        end
+
+        def get_workflow_title(workflow)
+          workflow.xpath('mediapackage/title/text()').to_s.strip
         end
 
         def convert_tech_metadata(workflow)
@@ -99,17 +113,29 @@ module ActiveEncode
         end
 
         def convert_output(workflow, options)
-          output = []
+          outputs = []
           workflow.xpath('//track[@type="presenter/delivery" and tags/tag[text()="streaming"]]').each do |track|
-            label = track.xpath('tags/tag[starts-with(text(),"quality")]/text()').to_s
-            url = track.at("url/text()").to_s
-            if url.start_with? "rtmp"
-              url = File.join(options[:stream_base], MatterhornRtmpUrl.parse(url).to_path) if options[:stream_base]
+            output = ActiveEncode::Output.new
+            output.label = track.xpath('tags/tag[starts-with(text(),"quality")]/text()').to_s
+            output.url = track.at("url/text()").to_s
+            if output.url.start_with? "rtmp"
+              output.url = File.join(options[:stream_base], MatterhornRtmpUrl.parse(output.url).to_path) if options[:stream_base]
             end
-            track_id = track.at("@id").to_s
-            output << convert_track_metadata(track).merge(id: track_id, url: url, label: label)
+            output.id = track.at("@id").to_s
+
+            tech_md = convert_track_metadata(track)
+            [:width, :height, :frame_rate, :duration, :checksum, :audio_codec, :video_codec,
+             :audio_bitrate, :video_bitrate, :file_size].each do |field|
+              output.send("#{field}=", tech_md[field])
+            end
+
+            output.state = :completed
+            output.created_at = convert_output_created_at(track, workflow)
+            output.updated_at = convert_output_updated_at(track, workflow)
+
+            outputs << output
           end
-          output
+          outputs
         end
 
         def convert_current_operations(workflow)
@@ -131,6 +157,18 @@ module ActiveEncode
           updated_at.present? ? Time.strptime(updated_at, "%Q") : nil
         end
 
+        def convert_output_created_at(track, workflow)
+          quality = track.xpath('tags/tag[starts-with(text(),"quality")]/text()').to_s
+          created_at = workflow.xpath("//operation[@id=\"compose\"][configurations/configuration[@key=\"target-tags\" and contains(text(), \"#{quality}\")]]/started/text()").to_s
+          created_at.present? ? Time.at(created_at.to_i / 1000.0) : nil
+        end
+
+        def convert_output_updated_at(track, workflow)
+          quality = track.xpath('tags/tag[starts-with(text(),"quality")]/text()').to_s
+          updated_at = workflow.xpath("//operation[@id=\"compose\"][configurations/configuration[@key=\"target-tags\" and contains(text(), \"#{quality}\")]]/completed/text()").to_s
+          updated_at.present? ? Time.at(updated_at.to_i / 1000.0) : nil
+        end
+
         def convert_options(workflow)
           options = {}
           options[:preset] = workflow.xpath('template/text()').to_s
@@ -141,20 +179,20 @@ module ActiveEncode
         def convert_track_metadata(track)
           return {} if track.nil?
           metadata = {}
-          metadata[:mime_type] = track.at("mimetype/text()").to_s if track.at('mimetype')
-          metadata[:checksum] = track.at("checksum/text()").to_s if track.at('checksum')
-          metadata[:duration] = track.at("duration/text()").to_s if track.at('duration')
+          # metadata[:mime_type] = track.at("mimetype/text()").to_s if track.at('mimetype')
+          metadata[:checksum] = track.at("checksum/text()").to_s.strip if track.at('checksum')
+          metadata[:duration] = track.at("duration/text()").to_s.to_i if track.at('duration')
           if track.at('audio')
             metadata[:audio_codec] = track.at("audio/encoder/@type").to_s
             metadata[:audio_channels] = track.at("audio/channels/text()").to_s
-            metadata[:audio_bitrate] = track.at("audio/bitrate/text()").to_s
+            metadata[:audio_bitrate] = track.at("audio/bitrate/text()").to_s.to_f
           end
           if track.at('video')
             metadata[:video_codec] = track.at("video/encoder/@type").to_s
-            metadata[:video_bitrate] = track.at("video/bitrate/text()").to_s
-            metadata[:video_framerate] = track.at("video/framerate/text()").to_s
-            metadata[:width] = track.at("video/resolution/text()").to_s.split('x')[0]
-            metadata[:height] = track.at("video/resolution/text()").to_s.split('x')[1]
+            metadata[:video_bitrate] = track.at("video/bitrate/text()").to_s.to_f
+            metadata[:frame_rate] = track.at("video/framerate/text()").to_s.to_f
+            metadata[:width] = track.at("video/resolution/text()").to_s.split('x')[0].to_i
+            metadata[:height] = track.at("video/resolution/text()").to_s.split('x')[1].to_i
           end
           metadata
         end
--- spec/shared_specs/engine_adapter_specs.rb
+++ spec/shared_specs/engine_adapter_specs.rb
@@ -6,6 +6,9 @@ RSpec.shared_examples 'an ActiveEncode::EngineAdapter' do |*_flags|
     raise 'adapter must be set with `let(:canceled_job)`' unless defined? canceled_job
     raise 'adapter must be set with `let(:completed_job)`' unless defined? completed_job
     raise 'adapter must be set with `let(:failed_job)`' unless defined? failed_job
+    raise 'adapter must be set with `let(:completed_tech_metadata)`' unless defined? completed_tech_metadata
+    raise 'adapter must be set with `let(:completed_output)`' unless defined? completed_output
+    raise 'adapter must be set with `let(:failed_tech_metadata)`' unless defined? failed_tech_metadata
   end
 
   it { is_expected.to respond_to :create }
@@ -24,27 +27,28 @@ RSpec.shared_examples 'an ActiveEncode::EngineAdapter' do |*_flags|
     its(:percent_complete) { is_expected.to be < 100 }
     its(:errors) { is_expected.to be_empty }
     its(:created_at) { is_expected.to be_kind_of Time }
-    its(:updated_at) { is_expected.to be_nil }
+    its(:updated_at) { is_expected.to be_kind_of Time }
 
-    context 'input' do
-      subject { created_job.input }
-
-      its(:width) { is_expected.to be_blank }
-      its(:height) { is_expected.to be_blank }
-      its(:frame_rate) { is_expected.to be_blank }
-      its(:duration) { is_expected.to be_blank }
-      its(:file_size) { is_expected.to be_blank }
-      its(:checksum) { is_expected.to be_blank }
-      its(:audio_codec) { is_expected.to be_blank }
-      its(:video_codec) { is_expected.to be_blank }
-      its(:audio_bitrate) { is_expected.to be_blank }
-      its(:video_bitrate) { is_expected.to be_blank }
+    it 'input is a valid ActiveEncode::Input object' do
+      expect(subject.input).to be_a ActiveEncode::Input
+      expect(subject.input.id).not_to be_blank
+      expect(subject.input.url).not_to be_blank
+      expect(subject.input.created_at).to be_kind_of Time
+      expect(subject.input.updated_at).to be_kind_of Time
+      expect(subject.input.updated_at).to be >= subject.created_at
     end
 
-    context 'output' do
-      subject { created_job.output }
-
-      it { is_expected.to be_blank }
+    it 'output has only valid ActiveEncode::Output objects' do
+      expect(subject.output).to be_a Array
+      subject.output.each do |out|
+        expect(out).to be_a ActiveEncode::Output
+        expect(out.id).not_to be_blank
+        expect(out.url).not_to be_blank
+        expect(out.label).not_to be_blank
+        expect(out.created_at).to be_kind_of Time
+        expect(out.updated_at).to be_kind_of Time
+        expect(out.updated_at).to be >= subject.created_at
+      end
     end
   end
 
@@ -61,6 +65,28 @@ RSpec.shared_examples 'an ActiveEncode::EngineAdapter' do |*_flags|
       its(:errors) { is_expected.to be_empty }
       its(:created_at) { is_expected.to be_kind_of Time }
       its(:updated_at) { is_expected.to be > subject.created_at }
+
+      it 'input is a valid ActiveEncode::Input object' do
+        expect(subject.input).to be_a ActiveEncode::Input
+        expect(subject.input.id).not_to be_blank
+        expect(subject.input.url).not_to be_blank
+        expect(subject.input.created_at).to be_kind_of Time
+        expect(subject.input.updated_at).to be_kind_of Time
+        expect(subject.input.updated_at).to be >= subject.created_at
+      end
+
+      it 'output has only valid ActiveEncode::Output objects' do
+        expect(subject.output).to be_a Array
+        subject.output.each do |out|
+          expect(out).to be_a ActiveEncode::Output
+          expect(out.id).not_to be_blank
+          expect(out.url).not_to be_blank
+          expect(out.label).not_to be_blank
+          expect(out.created_at).to be_kind_of Time
+          expect(out.updated_at).to be_kind_of Time
+          expect(out.updated_at).to be >= subject.created_at
+        end
+      end
     end
 
     context "a cancelled encode" do
@@ -75,7 +101,28 @@ RSpec.shared_examples 'an ActiveEncode::EngineAdapter' do |*_flags|
       its(:errors) { is_expected.to be_empty }
       its(:created_at) { is_expected.to be_kind_of Time }
       its(:updated_at) { is_expected.to be >= subject.created_at }
-      # its(:tech_metadata) { is_expected.to be_empty }
+
+      it 'input is a valid ActiveEncode::Input object' do
+        expect(subject.input).to be_a ActiveEncode::Input
+        expect(subject.input.id).not_to be_blank
+        expect(subject.input.url).not_to be_blank
+        expect(subject.input.created_at).to be_kind_of Time
+        expect(subject.input.updated_at).to be_kind_of Time
+        expect(subject.input.updated_at).to be >= subject.created_at
+      end
+
+      it 'output has only valid ActiveEncode::Output objects' do
+        expect(subject.output).to be_a Array
+        subject.output.each do |out|
+          expect(out).to be_a ActiveEncode::Output
+          expect(out.id).not_to be_blank
+          expect(out.url).not_to be_blank
+          expect(out.label).not_to be_blank
+          expect(out.created_at).to be_kind_of Time
+          expect(out.updated_at).to be_kind_of Time
+          expect(out.updated_at).to be >= subject.created_at
+        end
+      end
     end
 
     context "a completed encode" do
@@ -86,12 +133,43 @@ RSpec.shared_examples 'an ActiveEncode::EngineAdapter' do |*_flags|
       end
       its(:id) { is_expected.to eq 'completed-id' }
       it { is_expected.to be_completed }
-      its(:output) { is_expected.to eq completed_output }
       its(:percent_complete) { is_expected.to eq 100 }
       its(:errors) { is_expected.to be_empty }
       its(:created_at) { is_expected.to be_kind_of Time }
       its(:updated_at) { is_expected.to be > subject.created_at }
-      # its(:tech_metadata) { is_expected.to include completed_tech_metadata }
+
+      it 'input is a valid ActiveEncode::Input object' do
+        expect(subject.input).to be_a ActiveEncode::Input
+        expect(subject.input.id).not_to be_blank
+        expect(subject.input.url).not_to be_blank
+        expect(subject.input.created_at).to be_kind_of Time
+        expect(subject.input.updated_at).to be_kind_of Time
+        expect(subject.input.updated_at).to be >= subject.created_at
+      end
+
+      it 'input has technical metadata' do
+        expect(subject.input.as_json).to include completed_tech_metadata
+      end
+
+      it 'output has only valid ActiveEncode::Output objects' do
+        expect(subject.output).to be_a Array
+        subject.output.each do |out|
+          expect(out).to be_a ActiveEncode::Output
+          expect(out.id).not_to be_blank
+          expect(out.url).not_to be_blank
+          expect(out.label).not_to be_blank
+          expect(out.created_at).to be_kind_of Time
+          expect(out.updated_at).to be_kind_of Time
+          expect(out.updated_at).to be >= subject.created_at
+        end
+      end
+
+      it 'output has technical metadata' do
+        subject.output.each do |output|
+          expected_output = completed_output.find {|expected_out| expected_out[:id] == output.id }
+          expect(output.as_json.symbolize_keys).to include expected_output
+        end
+      end
     end
 
     context "a failed encode" do
@@ -106,7 +184,32 @@ RSpec.shared_examples 'an ActiveEncode::EngineAdapter' do |*_flags|
       its(:errors) { is_expected.not_to be_empty }
       its(:created_at) { is_expected.to be_kind_of Time }
       its(:updated_at) { is_expected.to be > subject.created_at }
-      # its(:tech_metadata) { is_expected.to include failed_tech_metadata }
+
+      it 'input is a valid ActiveEncode::Input object' do
+        expect(subject.input).to be_a ActiveEncode::Input
+        expect(subject.input.id).not_to be_blank
+        expect(subject.input.url).not_to be_blank
+        expect(subject.input.created_at).to be_kind_of Time
+        expect(subject.input.updated_at).to be_kind_of Time
+        expect(subject.input.updated_at).to be >= subject.created_at
+      end
+
+      it 'input has technical metadata' do
+        expect(subject.input.as_json.symbolize_keys).to include failed_tech_metadata
+      end
+
+      it 'output has only valid ActiveEncode::Output objects' do
+        expect(subject.output).to be_a Array
+        subject.output.each do |out|
+          expect(out).to be_a ActiveEncode::Output
+          expect(out.id).not_to be_blank
+          expect(out.url).not_to be_blank
+          expect(out.label).not_to be_blank
+          expect(out.created_at).to be_kind_of Time
+          expect(out.updated_at).to be_kind_of Time
+          expect(out.updated_at).to be >= subject.created_at
+        end
+      end
     end
   end
 
@@ -122,7 +225,28 @@ RSpec.shared_examples 'an ActiveEncode::EngineAdapter' do |*_flags|
     its(:errors) { is_expected.to be_empty }
     its(:created_at) { is_expected.to be_kind_of Time }
     its(:updated_at) { is_expected.to be >= subject.created_at }
-    # its(:tech_metadata) { is_expected.to be_empty }
+
+    it 'input is a valid ActiveEncode::Input object' do
+      expect(subject.input).to be_a ActiveEncode::Input
+      expect(subject.input.id).not_to be_blank
+      expect(subject.input.url).not_to be_blank
+      expect(subject.input.created_at).to be_kind_of Time
+      expect(subject.input.updated_at).to be_kind_of Time
+      expect(subject.input.updated_at).to be >= subject.created_at
+    end
+
+    it 'output has only valid ActiveEncode::Output objects' do
+      expect(subject.output).to be_a Array
+      subject.output.each do |out|
+        expect(out).to be_a ActiveEncode::Output
+        expect(out.id).not_to be_blank
+        expect(out.url).not_to be_blank
+        expect(out.label).not_to be_blank
+        expect(out.created_at).to be_kind_of Time
+        expect(out.updated_at).to be_kind_of Time
+        expect(out.updated_at).to be >= subject.created_at
+      end
+    end
   end
 
   describe "reload" do
@@ -137,5 +261,27 @@ RSpec.shared_examples 'an ActiveEncode::EngineAdapter' do |*_flags|
     its(:errors) { is_expected.to be_empty }
     its(:created_at) { is_expected.to be_kind_of Time }
     its(:updated_at) { is_expected.to be > subject.created_at }
+
+    it 'input is a valid ActiveEncode::Input object' do
+      expect(subject.input).to be_a ActiveEncode::Input
+      expect(subject.input.id).not_to be_blank
+      expect(subject.input.url).not_to be_blank
+      expect(subject.input.created_at).to be_kind_of Time
+      expect(subject.input.updated_at).to be_kind_of Time
+      expect(subject.input.updated_at).to be >= subject.created_at
+    end
+
+    it 'output has only valid ActiveEncode::Output objects' do
+      expect(subject.output).to be_a Array
+      subject.output.each do |out|
+        expect(out).to be_a ActiveEncode::Output
+        expect(out.id).not_to be_blank
+        expect(out.url).not_to be_blank
+        expect(out.label).not_to be_blank
+        expect(out.created_at).to be_kind_of Time
+        expect(out.updated_at).to be_kind_of Time
+        expect(out.updated_at).to be >= subject.created_at
+      end
+    end
   end
 end
