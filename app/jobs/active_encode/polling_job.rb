c185cfca6b6ad9784f7ac1e5b35d8c4f5b45021a
Chris Colvard <cjcolvar@indiana.edu>
2018-09-17 12:21:15 -0400

    Fix callbacks to match encode states used by adapters.

--- app/jobs/active_encode/polling_job.rb
+++ app/jobs/active_encode/polling_job.rb
@@ -4,12 +4,12 @@ module ActiveEncode
     def perform(encode)
       encode.run_callbacks(:status_update) { encode }
       case encode.state
-      when :error
-        encode.run_callbacks(:error) { encode }
+      when :failed
+        encode.run_callbacks(:failed) { encode }
       when :cancelled
         encode.run_callbacks(:cancelled) { encode }
-      when :complete
-        encode.run_callbacks(:complete) { encode }
+      when :completed
+        encode.run_callbacks(:completed) { encode }
       when :running
         ActiveEncode::PollingJob.set(wait: ActiveEncode::Polling::POLLING_WAIT_TIME).perform_later(encode)
       else # other states are illegal and ignored
