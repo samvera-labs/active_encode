1a705779a7f0ae4f87c0009f0d202e63e374ffde
Chris Colvard <cjcolvar@indiana.edu>
2018-12-17 21:35:41 -0500

    Test and fix controller

--- app/controllers/active_encode/encode_record_controller.rb
+++ app/controllers/active_encode/encode_record_controller.rb
@@ -2,9 +2,7 @@ module ActiveEncode
   class EncodeRecordController < ActionController::Base
     def show
       @encode_record = ActiveEncode::EncodeRecord.find(params[:id])
-      respond_to do |format|
-        format.any { render json: @encode_record.raw_object }
-      end
+      render json: @encode_record.raw_object
     end
   end
 end
