d02e146e1c5de21e2101ebef38ba58dfc67da509
Mason Ballengee <masaball@iu.edu>
2026-04-15 14:21:41 -0400

    Increase muxing queue size
    There is an issue in CI where one of the files is failing in ffmpeg
    because of a buffer memory error for one of the streams. Searching
    around indicated that the error is caused by a desync between the
    processing of the streams. FFmpeg will not start to actually process the
    file until it has bits from each stream, otherwise it will keep
    buffering one stream until it receives something from the other. If the
    buffer fills up, the processing fails. Bumping the muxing queue size is
    the main recommended approach for resolving the error.

--- lib/active_encode/engine_adapters/ffmpeg_adapter.rb
+++ lib/active_encode/engine_adapters/ffmpeg_adapter.rb
@@ -290,7 +290,7 @@ module ActiveEncode
           "#{k}: #{v}\r\n"
         end.join
         header_opt = "-headers '#{header_opt}'" if header_opt.present?
-        "#{FFMPEG_PATH} #{header_opt} -y -loglevel level+error -progress #{working_path('progress', id)} -i \"#{input_url}\" #{supplemental_file_opt} #{output_opt}"
+        "#{FFMPEG_PATH} #{header_opt} -y -loglevel level+error -progress #{working_path('progress', id)} -i \"#{input_url}\" -max_muxing_queue_size 1024 #{supplemental_file_opt} #{output_opt}"
       end
 
       def caption_extraction_options(filename, count, id)
